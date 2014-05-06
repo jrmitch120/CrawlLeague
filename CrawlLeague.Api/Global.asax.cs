05aebbf9a6ede24469fdb0cb21868b47500be678
jmitchell <jmitchell@verascape.com>
2014-05-06 13:18:14 -0500

    Initial Commit

---
+++ CrawlLeague.Api/Global.asax.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using CrawlLeague.ServiceInterface;
+using CrawlLeague.ServiceModel;
+using Funq;
+using ServiceStack;
+using ServiceStack.Configuration;
+using ServiceStack.Data;
+using ServiceStack.OrmLite;
+
+namespace CrawlLeague.Api
+{
+    public class AppHost : AppHostBase
+    {
+
+        public AppHost() : base("REST Files", typeof(TestService).Assembly) { }
+        
+        public override void Configure(Container container)
+        {
+            Plugins.Add(new CorsFeature());
+
+            SetConfig(new HostConfig
+            {
+                DebugMode = true,
+            });
+
+            var config = new AppConfig(new AppSettings());
+            
+            container.Register(config);
+
+            container.Register<IDbConnectionFactory>(
+                new OrmLiteConnectionFactory(
+                "Server=127.0.0.1;Port=5432;User Id=postgres;Password=test123;Database=testDb;Pooling=true;MinPoolSize=0;MaxPoolSize=200",
+                PostgreSqlDialect.Provider));
+
+            using (var db = container.Resolve<IDbConnectionFactory>().Open())
+            {
+                db.DropAndCreateTable<Test>();
+            }
+        }
+    }
+
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            //Initialize your application
+            (new AppHost()).Init();
+        }
+    }
+}
---
+++ CrawlLeague.ServiceInterface/AppConfig.cs
@@ -0,0 +1,16 @@
+﻿using ServiceStack.Configuration;
+
+
+namespace CrawlLeague.ServiceInterface
+{
+    public class AppConfig
+    {
+        public AppConfig()
+        {
+        }
+
+        public AppConfig(IAppSettings resources)
+        {
+        }
+    }
+}
