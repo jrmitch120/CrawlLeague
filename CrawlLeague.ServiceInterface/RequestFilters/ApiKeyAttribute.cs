247c8906241fe87e288caf50916be2bdb0366d74
Jeff Mitchell <jmitchell@verascape.com>
2014-05-08 16:25:52 -0500

    Apikey header modification.

--- CrawlLeague.ServiceInterface/RequestFilters/ApiKeyAttribute.cs
+++ CrawlLeague.ServiceInterface/RequestFilters/ApiKeyAttribute.cs
@@ -8,10 +8,10 @@ namespace CrawlLeague.ServiceInterface.RequestFilters
         public override void Execute(IRequest req, IResponse res, object requestDto)
         {
             var appConfig = req.TryResolve<AppConfig>();
-            var apiKey = req.Headers["X-ApiKey"] ?? req.QueryString["api_key"];
+            var apiKey = req.Headers["x-api-key"] ?? req.QueryString["api_key"];
             if (apiKey == null || !appConfig.ReadWriteApiKeys.Contains(apiKey))
             {
-                throw HttpError.Unauthorized("Unauthorized.  Valid X-ApiKey header required.");
+                throw HttpError.Unauthorized("Unauthorized.  Valid x-api-key header required.");
             } 
         }
     }
